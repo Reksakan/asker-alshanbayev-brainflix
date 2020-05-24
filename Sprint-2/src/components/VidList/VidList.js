f93ac98ba9b3f00b85e1a6ba33dbeccb5cdebaf7
unknown <aalshanbayev@gmail.com>
2020-05-23 22:16:13 -0400

    BrainFlix. Sprint-2. MainPage. Clicking to any Vid from Vidlist makes changes in URL (shows ID of the clicked Vid)

--- Sprint-2/src/components/VidList/VidList.js
+++ Sprint-2/src/components/VidList/VidList.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import './VidList.scss'
+import './VidList.scss';
+import { Link } from 'react-router-dom';
 
 const VidList = ({listOfVid}) => {  
   
   const array = listOfVid.map(vid => {
     return (
-      <section className="video" key={vid.id}>
-        <img className="video__img" src={vid.image} alt={vid.image}></img>
-        <div className="video__text">
-          <span className="video__text-title">{vid.title}</span>
-          <span className="video__text-author">{vid.channel}</span>
-        </div>
-      </section>
+      <Link to={"/"+vid.id}>
+        <section className="video" key={vid.id}>
+          <img className="video__img" src={vid.image} alt={vid.image}></img>
+          <div className="video__text">
+            <span className="video__text-title">{vid.title}</span>
+            <span className="video__text-author">{vid.channel}</span>
+          </div>
+        </section>
+      </Link>
     )
   })
   return (<div>{array}</div>)
--- Sprint-2/src/pages/HomePage.js
+++ Sprint-2/src/pages/HomePage.js
@@ -52,9 +52,9 @@ class HomePage extends React.Component {
     })
   }
 
-  // componentDidUpdate(prevProps, prevState) {
-
-  // }
+  componentDidUpdate(prevProps, prevState) {
+    
+  }
 
   render() {
     
